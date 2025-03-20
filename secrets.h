912bd091fb47d7ced9fc470d8cf6074f52c07a69
baltichomesteaders <baltichomesteaders@gmail.com>
2025-03-20 10:25:26 +0200

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,6 @@
+//wifi settings 
+const char* ssid     = "**"; // ESP32 and ESP8266 uses 2.4GHZ wifi only
+const char* password = "***"; 
+#define mqtt_server "***"
+#define mqtt_user "***"
+#define mqtt_pass "***"
