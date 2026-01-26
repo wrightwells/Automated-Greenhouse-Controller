8901e4cb57fa02c05ceedd27f6f9d30df70a8e85
wrightwells <wrightwells@hotmail.com>
2026-01-26 16:10:01 +0000

    refactored

--- secrets.example.h
+++
@@ -1,6 +0,0 @@
-//wifi settings 
-const char* ssid     = "***"; // ESP32 and ESP8266 uses 2.4GHZ wifi only
-const char* password = "***"; 
-#define mqtt_server "***"
-#define mqtt_user "***"
-#define mqtt_pass "***"
